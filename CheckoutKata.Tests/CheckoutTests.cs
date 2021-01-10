164f62b65cddf6a13242bd3a00b69cea62d7984b
Hani Samir <h.samir@edu.salford.ac.uk>
2021-01-10 19:53:48 +0000

    Unit test fails for get total with deductions for checkout

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -3,6 +3,7 @@ using CheckoutKata.Interfaces;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace CheckoutKata.Tests
 {
@@ -11,6 +12,7 @@ namespace CheckoutKata.Tests
         private Checkout _checkout;
         private Mock<IProduct> _mockProduct;
         private Mock<ISpecialPrice> _mockSpecialPrice;
+        private IList<Mock<IProduct>> _mockListProducts;
 
         [SetUp]
         public void Setup()
@@ -18,6 +20,18 @@ namespace CheckoutKata.Tests
             _checkout = new Checkout();
             _mockProduct = new Mock<IProduct>();
             _mockSpecialPrice = new Mock<ISpecialPrice>();
+
+            _mockProduct.SetupAllProperties();
+            _mockSpecialPrice.SetupAllProperties();
+
+            _mockSpecialPrice.Object.SpecialPriceID = 1;
+            _mockSpecialPrice.Object.SpecialPriceOffer = 130;
+            _mockSpecialPrice.Object.SpecialPriceQuantity = 3;
+
+            _mockProduct.Object.ProdID = 1;
+            _mockProduct.Object.ProdName = "A";
+            _mockProduct.Object.ProdPrice = 50;
+            _mockProduct.Object.SpecialPrice = _mockSpecialPrice.Object;
 
         }
 
@@ -31,41 +45,76 @@ namespace CheckoutKata.Tests
         [Test]
         public void WhenTotalPriceIsCalledToReturnThePrice()
         {
-            _mockSpecialPrice.Object.SpecialPriceID = 1;
-            _mockSpecialPrice.Object.SpecialPriceOffer = 45;
-            _mockSpecialPrice.Object.SpecialPriceQuantity = 2;
-
-            _mockProduct.Object.ProdID = 1;
-            _mockProduct.Object.ProdName = "A";
-            _mockProduct.Object.ProdPrice = 50;
-            _mockProduct.Object.SpecialPrice = _mockSpecialPrice.Object;
-
-
+        
             _checkout.ScanItem(_mockProduct.Object);
             var result = _checkout.GetTotalPrice();
             Assert.IsInstanceOf(typeof(int),result);
         }
 
         [Test]
-        public void WhenGetTotalPriceReturnsTheCorrectPrice()
+        public void WhenGetTotalPriceReturnsTheCorrectPriceForOneItem()
         {
-            _mockProduct.SetupAllProperties();
-            _mockSpecialPrice.SetupAllProperties();
-
-            _mockSpecialPrice.Object.SpecialPriceID = 1;
-            _mockSpecialPrice.Object.SpecialPriceOffer = 45;
-            _mockSpecialPrice.Object.SpecialPriceQuantity = 2;
-
-            
-            _mockProduct.Object.ProdID = 1;
-            _mockProduct.Object.ProdName = "A";
-            _mockProduct.Object.ProdPrice = 50;
-            _mockProduct.Object.SpecialPrice = _mockSpecialPrice.Object;
-
             _checkout.ScanItem(_mockProduct.Object);
             var result = _checkout.GetTotalPrice();
 
             Assert.AreEqual(50, result);
+        }
+
+        [Test]
+        public void WhenGetTotalPriceReturnsTheCorrectPriceForMultipleItem()
+        {
+            var itemTwo = new Mock<IProduct>();
+            var itemThree = new Mock<IProduct>();
+
+            itemTwo.SetupAllProperties();
+            itemThree.SetupAllProperties();
+
+            itemTwo.Object.ProdID = 2;
+            itemTwo.Object.ProdName = "B";
+            itemTwo.Object.ProdPrice = 30;
+            itemTwo.Object.SpecialPrice = null;
+
+            itemThree.Object.ProdID = 3;
+            itemThree.Object.ProdName = "C";
+            itemThree.Object.ProdPrice = 20;
+            itemThree.Object.SpecialPrice = null;
+
+            _checkout.ScanItem(_mockProduct.Object);
+            _checkout.ScanItem(itemTwo.Object);
+            _checkout.ScanItem(itemThree.Object);
+            var result = _checkout.GetTotalPrice();
+
+            Assert.AreEqual(100, result);
+        }
+
+        [Test]
+        public void WhenGetTotalPriceReturnsPriceWithDeductions()
+        {
+            var itemTwo = new Mock<IProduct>();
+            var itemThree = new Mock<IProduct>();
+
+            itemTwo.SetupAllProperties();
+            itemThree.SetupAllProperties();
+
+            itemTwo.Object.ProdID = 1;
+            itemTwo.Object.ProdName = "A";
+            itemTwo.Object.ProdPrice = 50;
+            itemTwo.Object.SpecialPrice = _mockSpecialPrice.Object;
+
+            itemThree.Object.ProdID = 1;
+            itemThree.Object.ProdName = "A";
+            itemThree.Object.ProdPrice = 50;
+            itemThree.Object.SpecialPrice = _mockSpecialPrice.Object;
+
+            _checkout.ScanItem(_mockProduct.Object);
+            _checkout.ScanItem(itemTwo.Object);
+            _checkout.ScanItem(itemThree.Object);
+            var result = _checkout.GetTotalPrice();
+
+            Assert.AreEqual(130, result);
+
+
+
         }
     }
 }
